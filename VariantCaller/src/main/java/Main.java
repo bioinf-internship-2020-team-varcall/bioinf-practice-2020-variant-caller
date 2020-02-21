a3052e73f074dd0681b92a96798dd11523fa9fbf
iamnzrv <nzrvofficial@gmail.com>
2020-02-21 17:43:22 +0300

    configured branch

--- VariantCaller/src/main/java/Main.java
+++ VariantCaller/src/main/java/Main.java
@@ -3,7 +3,7 @@ import joptsimple.*;
 public class Main {
 
     public static void main(String[] args) {
-        //program arguments are set in configurations(if you are using intellij idea)
+        //program arguments are set in configurations(if you are using intelliJ IDEA)
         CommandLineParser myParser = new CommandLineParser(args);
         System.out.println(myParser.getA());
         System.out.println(myParser.getB());
