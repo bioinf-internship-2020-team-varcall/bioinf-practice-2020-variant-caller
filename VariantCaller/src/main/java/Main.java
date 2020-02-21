c483048453630620386cc072cdfc45bb0174a9c6
iamnzrv <nzrvofficial@gmail.com>
2020-02-21 17:38:46 +0300

    renamed branch

--- VariantCaller/src/main/java/Main.java
+++ VariantCaller/src/main/java/Main.java
@@ -2,8 +2,8 @@ import joptsimple.*;
 
 public class Main {
 
-    //program arguments are set in configurations(if you are using intellij idea)
     public static void main(String[] args) {
+        //program arguments are set in configurations(if you are using intellij idea)
         CommandLineParser myParser = new CommandLineParser(args);
         System.out.println(myParser.getA());
         System.out.println(myParser.getB());
