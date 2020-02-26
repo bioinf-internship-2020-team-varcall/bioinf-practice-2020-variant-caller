70e621ecbb1d2c055f5d9e810df0753a08382e89
iamnzrv <nzrvofficial@gmail.com>
2020-02-26 12:43:38 +0300

    solved build issue

--- VariantCaller/src/main/java/com/epam/bioinf/variantcaller/CommandLineParser.java
+++ VariantCaller/src/main/java/com/epam/bioinf/variantcaller/CommandLineParser.java
@@ -8,22 +8,22 @@ import java.util.List;
 
 //This is temporary example command line parser, which will be changed in the future versions
 public class CommandLineParser {
-    private OptionParser parser;
-    private OptionSpec<String> a;
-    private OptionSet options;
+  private OptionParser parser;
+  private OptionSpec<String> a;
+  private OptionSet options;
 
-    public CommandLineParser(String[] args) throws Exception {
-        parser = new OptionParser();
-        parser.accepts("a");
-        a = parser.accepts("a").withRequiredArg();
-        options = parser.parse(args);
-    }
+  public CommandLineParser(String[] args) throws Exception {
+    parser = new OptionParser();
+    parser.accepts("a");
+    a = parser.accepts("a").withRequiredArg();
+    options = parser.parse(args);
+  }
 
-    public OptionSet getOptions() {
-        return options;
-    }
+  public OptionSet getOptions() {
+    return options;
+  }
 
-    public List<String> getA() {
-        return options.valuesOf(a);
-    }
+  public List<String> getA() {
+    return options.valuesOf(a);
+  }
 }
