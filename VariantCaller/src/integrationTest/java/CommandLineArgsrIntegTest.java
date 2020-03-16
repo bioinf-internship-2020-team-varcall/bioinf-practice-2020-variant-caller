a341ee42dd8def64353356ddfbf541d5b3dee062
iamnzrv <nzrvofficial@gmail.com>
2020-03-17 01:01:35 +0300

    updated project according to spotbugs and checkstyle

--- VariantCaller/src/integrationTest/java/CommandLineArgsrIntegTest.java
+++ VariantCaller/src/integrationTest/java/CommandLineArgsrIntegTest.java
@@ -15,6 +15,7 @@ import static java.io.File.pathSeparatorChar;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,12 +39,14 @@ public class CommandLineArgsrIntegTest {
   @Test
   public void programMustFailWithInvalidArguments() throws IOException, InterruptedException {
     String[] invalidTestArgs = {
-        "--fasta", integTestFilePath("test1.fasta") + pathSeparatorChar + integTestFilePath("test2.fasta"),
+        "--fasta", integTestFilePath("test1.fasta") + pathSeparatorChar
+          + integTestFilePath("test2.fasta"),
         "--bed", integTestFilePath("test1.bed"),
         "--sam", integTestFilePath("test1.sam")
     };
     ProcessInfo processInfo = launchProcessWithArgs(invalidTestArgs);
-    assertEquals("Exception in thread \"main\" java.lang.IllegalArgumentException: " + FASTA_ARGS_COUNT_EXC, processInfo.errorString);
+    assertEquals("Exception in thread \"main\" java.lang.IllegalArgumentException: "
+        + FASTA_ARGS_COUNT_EXC, processInfo.errorString);
     assertEquals(1, processInfo.exitValue);
   }
 
@@ -54,7 +57,9 @@ public class CommandLineArgsrIntegTest {
    * @return the string which holds error if process creates one or is empty otherwise
    * @throws IOException the exception which is thrown when process fails to launch
    */
-  private ProcessInfo launchProcessWithArgs(String[] args) throws IOException, InterruptedException {
+  private ProcessInfo launchProcessWithArgs(String[] args) throws
+      IOException,
+      InterruptedException {
     List<String> command = new ArrayList<>();
     command.add("java");
     command.add("-jar");
@@ -63,9 +68,13 @@ public class CommandLineArgsrIntegTest {
 
     ProcessBuilder builder = new ProcessBuilder(command);
     Process p = builder.start();
-    BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+    BufferedReader error = new BufferedReader(
+        new InputStreamReader(p.getErrorStream(), StandardCharsets.UTF_8)
+    );
     p.waitFor();
-    return new ProcessInfo(p.exitValue(), error.readLine());
+    String errorLine = error.readLine();
+    error.close();
+    return new ProcessInfo(p.exitValue(), errorLine);
   }
 
   private static final class ProcessInfo {
