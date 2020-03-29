073f7801da3ef9f53fe81cf0857115a0b1ecc337
Georgii <corradowave1@gmail.com>
2020-03-29 15:28:03 +0300

    Update Main.java

--- VariantCaller/src/main/java/com/epam/bioinf/variantcaller/Main.java
+++ VariantCaller/src/main/java/com/epam/bioinf/variantcaller/Main.java
@@ -3,21 +3,16 @@ package com.epam.bioinf.variantcaller;
 import com.epam.bioinf.variantcaller.cmdline.CommandLineParser;
 import com.epam.bioinf.variantcaller.cmdline.ParsedArguments;
 import com.epam.bioinf.variantcaller.handlers.SamHandler;
-import org.apache.commons.compress.compressors.zstandard.ZstdCompressorOutputStream;
 
-import java.nio.file.Path;
-import java.util.Map;
+import static com.epam.bioinf.variantcaller.helpers.TestHelper.testFilePath;
 
+/**
+ * Class launches program with command line arguments(implementation is
+ * is temporary and will be updated in future versions,
+ * now it is used for exception check in integration tests)
+ */
 public class Main {
   public static void main(String[] args) {
-    ParsedArguments parsedArguments = CommandLineParser.parse(args);
-
-    SamHandler handler = new SamHandler(parsedArguments.getSamPaths());
-    Map<Path, Long> map =  handler.computeReadsByPaths();
-    map.forEach((path, reads) -> System.out.println(path + " : " + reads));
-
-    System.out.println("Parsed fasta path = " + parsedArguments.getFastaPath());
-    System.out.println("Parsed bed paths = " + parsedArguments.getBedPaths());
-    System.out.println("Parsed sam paths = " + parsedArguments.getSamPaths());
+    CommandLineParser.parse(args);
   }
 }
