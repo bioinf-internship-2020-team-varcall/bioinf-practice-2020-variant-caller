fcbe960dc5db11fbf485fed0d965a52623a7de3d
Georgii <49598870+corradowaver@users.noreply.github.com>
2020-03-30 19:52:24 +1000

    Устранение технической накладки (#27)
    * init
    
    * Update build.gradle
    
    * 24-2
    
    * 25-3
    
    * Create Read_Sam_File.md
    
    * Update Read_Sam_File.md
    
    * Delete Read_Sam_File.md
    
    * Update test2.sam
    
    * 26-4
    
    * improved tests
    
    * files 27-5
    
    * Create SamHandlerMessages.java
    
    * 34-1
    
    * dumb commit
    
    * implementation 2.0
    
    * Update build.gradle
    
    * impl 2.1
    
    * Update Main.java
    
    * fixed
    
    * fixes by dima
    
    * javadoc 2.0
    
    * init
    
    * Update Main.java
    
    * Update build.gradle

--- VariantCaller/src/main/java/com/epam/bioinf/variantcaller/Main.java
+++ VariantCaller/src/main/java/com/epam/bioinf/variantcaller/Main.java
@@ -1,13 +1,14 @@
 package com.epam.bioinf.variantcaller;
 
 import com.epam.bioinf.variantcaller.cmdline.CommandLineParser;
-import com.epam.bioinf.variantcaller.cmdline.ParsedArguments;
 
+/**
+ * Class launches program with command line arguments(implementation is
+ * is temporary and will be updated in future versions,
+ * now it is used for exception check in integration tests)
+ */
 public class Main {
   public static void main(String[] args) {
-    ParsedArguments parsedArguments = CommandLineParser.parse(args);
-    System.out.println("Parsed fasta path = " + parsedArguments.getFastaPath());
-    System.out.println("Parsed bed paths = " + parsedArguments.getBedPaths());
-    System.out.println("Parsed sam paths = " + parsedArguments.getSamPaths());
+    CommandLineParser.parse(args);
   }
 }
