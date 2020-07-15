0d8404b8e6cc19b08a021b35c1c8f980c89a9c7b
Klim <klim.lestsenko@gmail.com>
2020-07-15 22:28:58 +0300

    extracting method and a few impovements

--- VariantCaller/src/main/java/com/epam/bioinf/variantcaller/caller/Caller.java
+++ VariantCaller/src/main/java/com/epam/bioinf/variantcaller/caller/Caller.java
@@ -15,13 +15,13 @@ public class Caller {
   private final HashMap<String, HashMap<Integer, PotentialVariants>> variants;
 
   public Caller(IndexedFastaSequenceFile fastaSequenceFile,
-      List<SAMRecord> samRecords) {
+                List<SAMRecord> samRecords) {
     variants = new HashMap<>();
     this.fastaSequenceFile = fastaSequenceFile;
     this.samRecords = samRecords;
   }
 
-  public ArrayList<Variant> call() {
+  public ArrayList<Variant> findVariants() {
     initVariants();
     ArrayList<Variant> variants = new ArrayList<>();
     this.variants.forEach((contigKey, contigValue) -> {
@@ -37,66 +37,69 @@ public class Caller {
 
   private void initVariants() {
     ProgressBar progressBar = new ProgressBar(samRecords.size());
-    samRecords.forEach(samRecord -> {
+    for (SAMRecord samRecord : samRecords) {
       String contig = samRecord.getContig();
-      if (contig != null) {
-
-        variants.compute(contig, (contigKey , variantsByContig) -> {
-          if (variantsByContig == null) {
-            variantsByContig = new HashMap<Integer, PotentialVariants>();
-          }
-
-          int start = samRecord.getStart();
-          ReferenceSequence subsequenceAt =
-              fastaSequenceFile.getSubsequenceAt(contig, start, samRecord.getEnd());
-          byte[] subsequenceBases = subsequenceAt.getBases();
-
-          int refInd = 0;
-          int readInd = 0;
-          byte[] readBases = samRecord.getReadBases();
-          for (CigarElement cigarElement : samRecord.getCigar().getCigarElements()) {
-            CigarOperator operator = cigarElement.getOperator();
-            int length = cigarElement.getLength();
-            if (operator.isAlignment()) {
-              for (int j = 0; j < length - 1; j++) {
-
-                int constReadInd = readInd;
-                int constRefInd = refInd;
-                int trueInd = start + refInd;
-
-                variantsByContig.compute(trueInd, (posKey, potentialVariant) -> {
-                  byte c = readBases[constReadInd];
-                  if (potentialVariant == null) {
-                    byte refChar = subsequenceBases[constRefInd];
-                    if (refChar != c) {
-                      potentialVariant = new PotentialVariants(refChar);
-                      potentialVariant.addPotentialVariant(c);
-                      return potentialVariant;
-                    } else {
-                      return null;
-                    }
-                  } else {
-                    if (potentialVariant.getRefChar() != c) {
-                      potentialVariant.addPotentialVariant(c);
-                    }
-                    return potentialVariant;
-                  }
-                });
-                refInd++;
-                readInd++;
-              }
-            } else if (operator.equals(CigarOperator.D)) {
-              refInd += length + 1;
-              readInd++;
-            } else if (operator.equals(CigarOperator.I)) {
-              readInd += length + 1;
-              refInd++;
-            }
-          }
-          return variantsByContig;
-        });
+      if (contig == null) {
+        continue;
       }
+      variants.compute(contig, (contigKey, variantsByContig) -> {
+        if (variantsByContig == null) {
+          variantsByContig = new HashMap<Integer, PotentialVariants>();
+        }
+        ReferenceSequence subsequenceAt =
+            fastaSequenceFile.getSubsequenceAt(contig, samRecord.getStart(), samRecord.getEnd());
+        byte[] subsequenceBases = subsequenceAt.getBases();
+        return processSingleRead(samRecord, variantsByContig, subsequenceBases);
+      });
       progressBar.process();
-    });
+    }
+  }
+
+  private HashMap<Integer, PotentialVariants> processSingleRead(SAMRecord samRecord,
+      HashMap<Integer, PotentialVariants> variantsByContig, byte[] subsequenceBases) {
+    int start = samRecord.getStart();
+    int refInd = 0;
+    int readInd = 0;
+    byte[] readBases = samRecord.getReadBases();
+    for (CigarElement cigarElement : samRecord.getCigar().getCigarElements()) {
+      CigarOperator operator = cigarElement.getOperator();
+      int length = cigarElement.getLength();
+      if (operator.isAlignment()) {
+        for (int j = 0; j < length - 1; j++) {
+
+          int constReadInd = readInd;
+          int constRefInd = refInd;
+          int trueInd = start + refInd;
+
+          variantsByContig.compute(trueInd, (posKey, potentialVariant) -> {
+            byte c = readBases[constReadInd];
+            if (potentialVariant == null) {
+              byte refChar = subsequenceBases[constRefInd];
+              if (refChar != c) {
+                potentialVariant = new PotentialVariants(refChar);
+                potentialVariant.addPotentialVariant(c);
+                return potentialVariant;
+              } else {
+                return null;
+              }
+            } else {
+              if (potentialVariant.getRefChar() != c) {
+                potentialVariant.addPotentialVariant(c);
+              }
+              return potentialVariant;
+            }
+          });
+          refInd++;
+          readInd++;
+        }
+      } else if (operator.equals(CigarOperator.D)) {
+        refInd += length + 1;
+        readInd++;
+      } else if (operator.equals(CigarOperator.I)) {
+        readInd += length + 1;
+        refInd++;
+      }
+    }
+    return variantsByContig;
   }
 }
