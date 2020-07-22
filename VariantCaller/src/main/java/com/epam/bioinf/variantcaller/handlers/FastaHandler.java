30dac31085cf71292d226ca60e7baafe4d994598
Klim <klim.lestsenko@gmail.com>
2020-07-22 20:07:10 +0300

    better warning handling

--- VariantCaller/src/main/java/com/epam/bioinf/variantcaller/handlers/FastaHandler.java
+++ VariantCaller/src/main/java/com/epam/bioinf/variantcaller/handlers/FastaHandler.java
@@ -3,9 +3,15 @@ package com.epam.bioinf.variantcaller.handlers;
 import com.epam.bioinf.variantcaller.cmdline.ParsedArguments;
 import com.epam.bioinf.variantcaller.exceptions.handlers.fasta.FastaHandlerUnableToFindEntryException;
 import htsjdk.samtools.SAMException;
-import htsjdk.samtools.reference.*;
+import htsjdk.samtools.reference.FastaSequenceIndex;
+import htsjdk.samtools.reference.FastaSequenceIndexCreator;
+import htsjdk.samtools.reference.IndexedFastaSequenceFile;
+import htsjdk.samtools.reference.ReferenceSequence;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Path;
 
 /**
  * Class holds a reference file with sequences and performs work with it.
@@ -16,26 +22,39 @@ public class FastaHandler {
 
   /**
    * Constructor creates indexed sequence file and stores it.
+   * Unavoidable AsciiLineReader warning is redirected to nullOutputStream
    * Throwable exception is handled by ParsedArguments
+   *
    * @param parsedArguments parsedArguments with validated path to fasta file
    * @see ParsedArguments
    */
   public FastaHandler(ParsedArguments parsedArguments) {
     try {
-      FastaSequenceIndex fastaSequenceIndex = FastaSequenceIndexCreator
-          .buildFromFasta(parsedArguments.getFastaPath());
-      fastaSequenceFile = new IndexedFastaSequenceFile(
-          parsedArguments.getFastaPath(), fastaSequenceIndex);
+      FastaSequenceIndex fastaSequenceIndex =
+          getSequenceIndexFileWithoutWarning(parsedArguments.getFastaPath());
+      fastaSequenceFile =
+          new IndexedFastaSequenceFile(parsedArguments.getFastaPath(), fastaSequenceIndex);
     } catch (IOException e) { // Handled by ParsedArguments
       e.printStackTrace();
     }
   }
 
+  private FastaSequenceIndex getSequenceIndexFileWithoutWarning(Path fastaPath)
+      throws IOException {
+    // Temporary redirection of AsciiLineReader warning
+    final PrintStream stdErr = System.err;
+    System.setErr(new PrintStream(OutputStream.nullOutputStream(), true, "utf-8"));
+    FastaSequenceIndex fastaSequenceIndex = FastaSequenceIndexCreator.buildFromFasta(fastaPath);
+    System.setErr(stdErr);
+    return fastaSequenceIndex;
+  }
+
   /**
    * Return sequence at specified contig and range
+   *
    * @param contig reference chromosome
-   * @param start range start index
-   * @param stop range end index
+   * @param start  range start index
+   * @param stop   range end index
    * @return ReferenceSequence
    */
   public ReferenceSequence getSubsequence(String contig, long start, long stop) {
