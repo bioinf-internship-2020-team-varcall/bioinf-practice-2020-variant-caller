7a195900c3535738581152fd5e562eca84df7c67
Georgii <49598870+corradowaver@users.noreply.github.com>
2020-07-29 20:53:48 +0300

    improved header

--- VariantCaller/src/main/java/com/epam/bioinf/variantcaller/handlers/VcfHandler.java
+++ VariantCaller/src/main/java/com/epam/bioinf/variantcaller/handlers/VcfHandler.java
@@ -4,23 +4,18 @@ import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.variantcontext.VariantContextBuilder;
 import htsjdk.variant.variantcontext.writer.VariantContextWriter;
 import htsjdk.variant.variantcontext.writer.VariantContextWriterBuilder;
-import htsjdk.variant.vcf.VCFHeader;
-import htsjdk.variant.vcf.VCFHeaderLineCount;
-import htsjdk.variant.vcf.VCFHeaderLineType;
-import htsjdk.variant.vcf.VCFInfoHeaderLine;
+import htsjdk.variant.vcf.*;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class VcfHandler {
   private static final String OUTPUT_FILE_NAME = "output.vcf";
 
   public VcfHandler(Optional<Path> outputFilePath, List<VariantContext> variants) {
     VariantContextWriter writer = configureVcfWriter(outputFilePath);
-    VCFHeader header = getHeader();
+    VCFHeader header = getHeader(variants);
     writer.writeHeader(header);
     List<VariantContext> variantContexts = getVariantContexts(variants);
     variantContexts.forEach(writer::add);
@@ -35,23 +30,40 @@ public class VcfHandler {
       var alleles = variant.getAlleles();
       var pos = variant.getStart();
       var end = variant.getEnd();
-      builder.alleles(alleles).start(pos).stop(end).chr(contig).unfiltered().noID();
+      var info = variant.getCommonInfo().getAttributes();
+      var genotypes = variant.getGenotypes();
+      builder.alleles(alleles).start(pos).stop(end).chr(contig).attributes(info).genotypes(genotypes);
       variantContexts.add(builder.make());
     });
 
     return variantContexts;
   }
 
-  private VCFHeader getHeader() {
-    VCFHeader header = new VCFHeader();
-    VCFInfoHeaderLine metaData = new VCFInfoHeaderLine(
-        "COVID-19",
-        VCFHeaderLineCount.UNBOUNDED,
-        VCFHeaderLineType.String,
-        "ULTIMATE VCF POWER."
-    );
-    header.addMetaDataLine(metaData);
-    return header;
+  private VCFHeader getHeader(List<VariantContext> variants) {
+
+    final Set<String> sampleSet = new HashSet<>();
+    variants.forEach(
+        variant -> sampleSet.addAll(variant.getSampleNames()));
+
+    final Set<VCFHeaderLine> metaData = new HashSet<>();
+    metaData.add(VCFStandardHeaderLines.getInfoLine(VCFConstants.ALLELE_COUNT_KEY));
+    metaData.add(VCFStandardHeaderLines.getInfoLine(VCFConstants.ALLELE_FREQUENCY_KEY));
+    metaData.add(VCFStandardHeaderLines.getInfoLine(VCFConstants.ALLELE_NUMBER_KEY));
+    metaData.add(VCFStandardHeaderLines.getFormatLine(VCFConstants.DEPTH_KEY));
+    metaData.add(VCFStandardHeaderLines.getFormatLine(VCFConstants.GENOTYPE_KEY));
+    metaData.add(new VCFFormatHeaderLine(
+        "DPG",
+        VCFHeaderLineCount.G,//one value of each genotype
+        VCFHeaderLineType.Integer,
+        "Depth for each allele"));
+    metaData.add(new VCFInfoHeaderLine(
+        "INDEL",
+        1,
+        VCFHeaderLineType.Flag,
+        "Variant is indel"));
+
+
+    return new VCFHeader(metaData, sampleSet);
   }
 
   private VariantContextWriter configureVcfWriter(Optional<Path> outputFilePath) {
